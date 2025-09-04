5bc44a8650681d301ef975e282d34c12b96fc66c
Estêvão <rodriguesfaria35@gmail.com>
2025-09-04 14:14:24 -0300

    testando dnv

--- db/seeds/usuarios.js
+++ db/seeds/usuarios.js
@@ -6,10 +6,12 @@ exports.seed = async function(knex) {
   // Limpar a tabela antes de inserir
   await knex('usuarios').del();
   
+  // Reset auto-increment sequence
+  await knex.raw('ALTER SEQUENCE usuarios_id_seq RESTART WITH 1');
+  
   // Inserir dados de exemplo
   await knex('usuarios').insert([
     {
-      id: 1,
       nome: 'Admin Sistema',
       email: 'admin@policia.gov.br',
       senha: '$2b$10$vnu2AYVK5ijpHbb3i5VEmOcI59dugxporAvOdStjI3WXfKTN3rlO.' // Admin@123
